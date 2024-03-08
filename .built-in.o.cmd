f5b477f5d3a4d97bdda32737ccbc45cbb0d71e90
mfxjx <mfxjx000205@163.com>
2024-03-08 18:33:02 +0800

    update .gitignore

--- .built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/chaihu/Matrix_ToF_Drones/Firmware/built-in.o :=  arm-none-eabi-gcc --specs=nosys.specs --specs=nano.specs -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16   -r -o /home/chaihu/Matrix_ToF_Drones/Firmware/built-in.o /home/chaihu/Matrix_ToF_Drones/Firmware/src/built-in.o
